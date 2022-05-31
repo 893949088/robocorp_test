694ad564cb1ec505544a742f2e213fa606f42eb2
yanhonghao <893949088@qq.com>
2022-05-31 09:38:34 +0800

    添加项目文件。

---
+++ py_robot/task.py
@@ -0,0 +1,8 @@
+"""Template robot with Python."""
+
+def minimal_task():
+    print("哈哈哈.")
+
+
+if __name__ == "__main__":
+    minimal_task()
---
+++ rpa_robot/tasks.robot
@@ -0,0 +1,7 @@
+*** Settings ***
+Documentation       Template robot main suite.
+
+
+*** Tasks ***
+测试任务
+    Log To Console    哈哈哈
